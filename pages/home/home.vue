36010775c25a4e4c5ab17cd4341123c736546424
柯贤波 <13026389969@163.com>
2022-07-16 16:15:40 +0800

    轮播图接口对接

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    首页
+     <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+       <swiper-item v-for="(item,i) in swiperList" :key="index">
+        <image :src="item.image_src" mode=""></image>
+       </swiper-item>
+     </swiper>
   </view>
 </template>
 
@@ -8,12 +12,36 @@
   export default {
     data() {
       return {
-        
+            swiperList: [],
       };
+    },
+   onLoad() {
+      // 2. 在小程序页面刚加载的时候，调用获取轮播图数据的方法
+      this.getSwiperList()
+    },
+    methods:{
+       // 3. 获取轮播图数据的方法
+          async getSwiperList() {
+            // 3.1 发起请求
+            const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+            // 3.2 请求失败
+            if (res.meta.status !== 200) {
+              return uni.showToast({
+                title: '数据请求失败！',
+                duration: 1500,
+                icon: 'none',
+              })
+            }
+            // 3.3 请求成功，为 data 中的数据赋值
+            this.swiperList = res.message
+          },
     }
   }
 </script>
 
 <style lang="scss">
+image{
+  width:100%;
+}
 
 </style>
